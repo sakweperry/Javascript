9f8134a623ccdd5b97560248b32873cc533f136a
sakweperry <sakweperry231@gmail.com>
2026-01-28 09:23:38 +0530

    SOMECCHAGE

---
+++ script.js
@@ -0,0 +1,48 @@
+
+
+
+// let vote=(age)=>
+// {
+//     if(age<10){
+//        age="infant";
+//     }
+//     else if(age>=10 && age<20){
+//          age="teenagers";
+//     }
+//     else if(age>=20 && age<30){
+//         age="adult";
+//     }
+//     else if(age>=30 && age<40){
+//         age="working"
+//     }
+//     else{
+//         age="old";
+//     }
+//     return age;
+// }
+// let votinage=vote(11);
+// console.log(votinage);
+
+// const title = document.getElementById('main-heading');
+// console.log(title);
+// const listItems = document.getElementsByClassName('list-items');
+// console.log(listItems);
+// const listItems = document.getElementsByTagName('li');
+// console.log(listItems);
+
+// const boy = document.querySelector('div');
+// console.log(boy);
+
+// styling elements
+// const title = document.querySelector('#main-heading');
+// title.style.color='red';
+// title.style.border='1px solid red';
+
+// const listItem=document.querySelectorAll('.list-items');
+// for(i=0; i<listItem.length; i++){
+//     listItem[i].style.fontSize='50px';
+// }
+
+const para = document.querySelector('#demo');
+para.innerHTML="Date: " + Date();
+document.write("hi cameroon");
