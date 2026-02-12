0f457fba3030604ff7b8910cab020b50c493e703
sakweperry <sakweperry231@gmail.com>
2026-02-12 21:04:42 +0530

    my commit

--- script.js
+++ script.js
@@ -43,6 +43,65 @@
 //     listItem[i].style.fontSize='50px';
 // }
 
-const para = document.querySelector('#demo');
-para.innerHTML="Date: " + Date();
-document.write("hi cameroon");
+// const para = document.querySelector('#demo');
+// para.innerHTML="Date: " + Date();
+// document.write("hi cameroon");
+
+// javascript object
+// let computer = {
+//    name : "hp",
+//    speed : 64,
+//    storage : 128,
+
+//    typing: function(){
+//     console.log("start typing");
+//    },
+//    video: function(){
+//     console.log("play video");
+//    },
+//    audio: function(){
+//     console.log("play audio");
+//    }
+// };
+// console.log(computer.name);
+// console.log(computer.speed);
+// console.log(computer.storage);
+
+// computer.typing();
+// computer.video();
+// computer.audio();
+
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+// const rice=document.createElement('li');
+// ul.append(rice);
+// ul.append(li);
+// rice.innerText="rice";
+// li.innerText="perry";
+// li.classList="list-items"
+// ul.childNodes[3].style.color='red';
+// console.log(ul);
+// const div=document.querySelector('div');
+// const p=document.createElement('p');
+// div.append(p);
+// p.innerText="how old are you?";
+
+// addEventListener
+// const button1 = document.querySelector('.btn1');
+// function alertBtn1(){
+//    alert('i love perry');
+// }
+// button1,addEventListener('click', alertBtn1);
+
+const revealBtn=document.querySelector('.reveal-btn');
+const hiddenConten=document.querySelector('.hidden-conten');
+
+function revealConten(){
+   if(hiddenConten.classList.contains('reveal-btn')){
+      hiddenConten.classList.remove('reveal-btn');
+   }
+   else{
+        hiddenConten.classList.add('reveal-btn');
+   }
+}
+revealBtn.addEventListener('click',revealConten );
